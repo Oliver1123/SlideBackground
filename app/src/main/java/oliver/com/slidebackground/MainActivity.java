14abb9b2c6ea8965803595a39d9606fdf1cd3d21
zolotar <zolotar1123@gmail.com>
2016-11-23 21:28:15 +0200

    update animation variables

--- app/src/main/java/oliver/com/slidebackground/MainActivity.java
+++ app/src/main/java/oliver/com/slidebackground/MainActivity.java
@@ -1,7 +1,8 @@
 package oliver.com.slidebackground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SlideBackgroundView slideBackgroundView = (SlideBackgroundView) findViewById(R.id.sbv_background_slides);
+
+        slideBackgroundView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (slideBackgroundView.isAnimationStarted()) {
+                    slideBackgroundView.pauseAnimation();
+                } else {
+                    slideBackgroundView.startAnimation(false);
+                }
+            }
+        });
+        slideBackgroundView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                slideBackgroundView.startAnimation(true);
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/oliver/com/slidebackground/SlideBackgroundView.java
+++ app/src/main/java/oliver/com/slidebackground/SlideBackgroundView.java
@@ -3,6 +3,8 @@ package oliver.com.slidebackground;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -16,13 +18,28 @@ public class SlideBackgroundView extends View {
     private static final int DEFAULT_UPDATE_DELAY = 1000 / 24;
     private static final String TAG = SlideBackgroundView.class.getSimpleName();
 
-    private int mUpdateDelay;
+    private static final int DEFAULT_ANIMATION_SPEED = 2;
+    private static final int MSG_INVALIDATE = 0;
+
+    private int mRedrawDelay;
     private int[] mImageId;
 
     private Bitmap mLeftImage;
     private Bitmap mRightImage;
 
+    private int mElapsedTime;
+    private int mAnimationSpeed;
+
     private boolean mAnimationStarted;
+    private final Handler mInvalidateHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mElapsedTime += mAnimationSpeed;
+            invalidate();
+            sendEmptyMessageDelayed(MSG_INVALIDATE, mRedrawDelay);
+        }
+    };
 
 
     public SlideBackgroundView(Context context) {
@@ -46,16 +63,27 @@ public class SlideBackgroundView extends View {
     }
 
     private void init() {
-        mUpdateDelay = DEFAULT_UPDATE_DELAY;
+        mRedrawDelay = DEFAULT_UPDATE_DELAY;
+        mAnimationSpeed = DEFAULT_ANIMATION_SPEED;
     }
 
 
-    public void startAnimation() {
+    public void startAnimation(boolean restart) {
+        Log.d(TAG, "startAnimation: ");
         mAnimationStarted = true;
+        if (restart) mElapsedTime = 0;
+        mInvalidateHandler.removeMessages(MSG_INVALIDATE);
+        mInvalidateHandler.sendEmptyMessage(MSG_INVALIDATE);
     }
 
     public void pauseAnimation() {
+        Log.d(TAG, "pauseAnimation: ");
         mAnimationStarted = false;
+        mInvalidateHandler.removeMessages(MSG_INVALIDATE);
+    }
+
+    public boolean isAnimationStarted() {
+        return mAnimationStarted;
     }
 
     public void setAnimatedImages(@DrawableRes int... imageID) {
@@ -66,8 +94,6 @@ public class SlideBackgroundView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Log.d(TAG, "onDraw: ");
-        if (mAnimationStarted)
-            postInvalidateDelayed(mUpdateDelay);
+        Log.d(TAG, "onDraw: time: " + mElapsedTime);
     }
 }
